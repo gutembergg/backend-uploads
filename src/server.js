66cd1a381227cb3b405e55d560b6bf6ec9ee8504
Gutembergue Mascarenhas <gmascarenhas3001@gmail.com>
2020-06-03 14:55:27 +0200

    final commit backend

--- src/server.js
+++ src/server.js
@@ -4,9 +4,11 @@ const express = require('express');
 const morgan = require('morgan');
 const PORT = 3001;
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
